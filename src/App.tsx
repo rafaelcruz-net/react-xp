cf76bf04f2e99cd0127aed109471028376f90273
Rafael Bento Cruz <rbcruz@icatuseguros.com.br>
2017-04-17 20:37:06 -0300

    ReactXP Sample

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+/*
+* This file demonstrates a basic ReactXP app.
+*/
+
+import RX = require('reactxp');
+
+const styles = {
+    container: RX.Styles.createViewStyle({
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5fcff'
+    }),
+    helloWorld: RX.Styles.createTextStyle({
+        fontSize: 48,
+        fontWeight: 'bold',
+        marginBottom: 28
+    }),
+};
+
+class App extends RX.Component<null, null> {
+    constructor() {
+        super();
+
+
+
+    }
+
+    render(): JSX.Element | null {
+        return (
+            <RX.View style={ styles.container }>
+                <RX.Text style={ [styles.helloWorld] }>
+                    Hello World
+                </RX.Text>
+            </RX.View>
+        );
+    }
+  
+}
+
+export = App;
